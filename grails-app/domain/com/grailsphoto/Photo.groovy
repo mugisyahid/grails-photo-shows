3638788cfcda61f9749db46d8eabdc234bc2ec16
Ligar Mugi Syahid <ligar@eunoia.asia>
2017-10-10 03:47:29 +0700

    add photo

---
+++ grails-app/domain/com/grailsphoto/Photo.groovy
@@ -0,0 +1,18 @@
+package com.grailsphoto
+
+class Photo {
+
+
+    String description
+
+    String path
+
+    String imageRaw
+
+    int heigh
+
+    int widht
+
+    static constraints = {
+    }
+}
---
+++ src/test/groovy/com/grailsphoto/PhotoSpec.groovy
@@ -0,0 +1,18 @@
+package com.grailsphoto
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class PhotoSpec extends Specification implements DomainUnitTest<Photo> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
